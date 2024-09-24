23131f82061cac75c914466611f836cd3351986e
Rebols <rebecca.olsson@knowit.se>
2024-09-24 11:02:49 -0700

    footer and scss added

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
     import { RouterLink, RouterView } from 'vue-router'
     //import NavBar from './components/NavBar/NavBar.vue'
     import CustomParticles from "./components/Particles/Particles.vue"
-    //import Footer from "./components/Footer/Footer.vue";
+    import Footer from "./components/Footer/Footer.vue";
 </script>
-<!--
+
 <style>
     @import './assets/main.scss';
 </style>
-    -->
+ 
 
 <template>
     <header>
@@ -16,7 +16,7 @@
     </header>
     <CustomParticles />
     <RouterView />
-    <!--<footer>
+    <footer>
         <Footer />
-    </footer>-->
+    </footer>
 </template>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import LinkedInIcon from '../icons/IconLinkedIn.vue';
+    import ContactIcon from '../icons/IconContact.vue';
+    import DownloadIcon from '../icons/IconDownload.vue';
+</script>
+<template>
+    <div class="footer">
+        <div class="footer-content">
+            <p>Designed and developed by Rebecca Olsson</p>
+
+            <a href="https://storyset.com/work">Work illustrations by Storyset</a>
+
+        </div>
+        <div class="contact">
+            <a href="https://www.linkedin.com/in/rebecca-olsson/" target="_blank" class="contact-icons" data-title="Visit Linked In"><LinkedInIcon /></a>
+            <a href="mailto:olsson_rebecca@outlook.com" class="contact-icons" data-title="Send E-mail"><ContactIcon /></a>            
+            <a href="CV.pdf" class="contact-icons" download="CV_Rebecca_Olsson" data-title="Download resume"><DownloadIcon /></a>
+        </div>
+    </div>
+</template>
+
+<style src="./Footer.scss"></style>
