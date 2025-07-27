16091a5b8ee71cd7d34b9d22cad45d29d813cba2
Rebols <olsson_rebecca@outlook.com>
2025-07-26 21:23:06 -0700

    experience added and small changes

--- src/components/SectionAbout/SectionAbout.vue
+++ src/components/SectionAbout/SectionAbout.vue
@@ -14,25 +14,30 @@
     <div class="about-section">
         <div class="about-information">
             <h1>Let me  <span class="turqoise-text">introduce</span> myself</h1>
-            <p>
-                My name is Rebecca Olsson, I am 29 years old and come from Sweden.<br />
-                I moved to Canada fall 2022 and I recently recieved my Permanent Residency.<br /><br />
-                I fell in love with coding when I was studying to a M.Sc. in Biomedical Engineering.<br />When I graduated I started to work as an IT Consultant within Web development at <a href="https://www.knowit.eu/" target="_blank" rel="noopener noreferrer">Knowit</a>. <br />
-                Combining my coding knowledge with my social skills is something I find very motivating.
-                <br /><br />
-                When I am not working I like to do outdoor activities such as skiing, downhill biking, hiking and camping.<br /> I'm also enjoying concerts (indie and rock), game nights with friends and baking!
-            </p>
+  <p>
+    My name is Rebecca Olsson. I’m 30 years old and originally from Sweden.<br />
+    I fell in love with coding while studying for my M.Sc. in Biomedical Engineering. <br />
+    After graduating, I began my career as an IT Consultant in Web Development at 
+    <a href="https://www.knowit.eu/" target="_blank" rel="noopener noreferrer">Knowit</a>. <br />
+    Combining my technical skills with my social strengths is something I find very motivating.
+                  <br /><br />
+    In the fall of 2022, I moved to Canada. After spending time securing my Permanent Residence, I began working as a 
+    Web Developer & Digital Marketing Specialist for Gibbons Whistler, where I independently maintain 7 websites.
+                 <br /><br />
+    Outside of work, I love outdoor activities such as skiing, downhill biking, hiking, and camping. I also enjoy concerts (especially indie and rock), game nights with friends, and baking!
+  </p>
+
         </div>
         <div class="facts-box">
             <h2>Quick facts</h2>
             <ul>
-                <li><CodeIcon />IT Consultant, Web Development</li>
+                <li><CodeIcon />IT Consultant, Web Developer</li>
                 <li><BookIcon />M.sc. in Biomedical Engineering</li>
                 <li><FlagIcon />Swedish Citizen, Canadian PR</li>
                 <li><AboutIcon />Structured, Reliable, Outgoing</li>
                 <li><HeartIcon />Coding, client contact, teamwork</li>
                 <li> <GeotagIcon />Whistler, Canada</li>
-                <li><BirthdayIcon />29 y/o</li>
+                <li><BirthdayIcon />30 y/o</li>
                 <!-- <li><FlagIcon />Swedish</li>-->
             </ul>
         </div>
--- src/components/SectionTools/SectionTools.vue
+++ src/components/SectionTools/SectionTools.vue
@@ -8,22 +8,27 @@
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg', name: 'CSS' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg', name: 'JavaScript' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg', name: 'Vue' },
-        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg', name: 'NodeJS' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg', name: 'Sass' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg', name: 'Bootstrap' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg', name: 'Webpack' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg', name: 'JQuery' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg', name: 'C#' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg', name: '.NET' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg', name: 'PHP' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg', name: 'NodeJS' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg', name: 'MongoDB' },
-        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg', name: 'Microsoft SQL' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg', name: 'MySQL' },
+        { src: '/images/Optimizely_Logo.png', name: 'Optimizely' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-plain.svg', name: 'WordPress' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg', name: 'Dart' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg', name: 'Flutter' },
-        { src: '/images/Optimizely_Logo.png', name: 'Optimizely' },
-        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg', name: 'Jira' },
-        { component: GithubIcon, name: 'Github' },
         { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg', name: 'Postman' },
-        { src: '/images/siteimprove.png', name: 'SiteImprove' }
+        { component: GithubIcon, name: 'Github' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg', name: 'Git' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cloudflare/cloudflare-original.svg', name: 'Cloudflare' },
+        { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg', name: 'Jira' },
+        { src: '/images/siteimprove.png', name: 'SiteImprove' },
+        
     ];
 </script>
 
